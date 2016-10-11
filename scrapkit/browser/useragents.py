8929934484f1ce0605b354fff46c1ffb0d1fecee
Ashwin-Surana <surana.an@gmail.com>
2016-10-11 18:13:01 +0530

    browser fixed

---
+++ scrapkit/browser/useragents.py
@@ -0,0 +1,8 @@
+
+USER_AGENTS = [
+    r'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:48.0) Gecko/20100101 Firefox/48.0',
+    r'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:48.0) Gecko/20100101 Firefox/46.0',
+    r'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2490.80 Safari/537.36',
+    r'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36',
+    r'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36'
+]
